7b4e2137917132576c7b13a05fc9f896607bb44f
Sfoucheur <s.foucheur@gmail.com>
2023-05-18 15:33:51 +0200

    Pushing new build gradle for release 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
     defaultConfig {
         applicationId = "com.sf.tadami"
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
