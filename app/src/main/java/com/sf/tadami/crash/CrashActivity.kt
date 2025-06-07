3deeb5b9bfb1d948ab6414537e3f3f2d1706f6f5
Sfoucheur <s.foucheur@gmail.com>
2025-06-07 11:41:14 +0200

    fix: crashactivity style

--- app/src/main/java/com/sf/tadami/crash/CrashActivity.kt
+++ app/src/main/java/com/sf/tadami/crash/CrashActivity.kt
@@ -3,8 +3,14 @@ package com.sf.tadami.crash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.sf.tadami.ui.main.MainActivity
+import com.sf.tadami.ui.themes.TadamiTheme
 import com.sf.tadami.ui.utils.setComposeContent
 
 
@@ -13,17 +19,37 @@ class CrashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
         val exception = GlobalExceptionHandler.getThrowableFromIntent(intent)
         setComposeContent {
-            CrashScreen(
-                exception = exception,
-                onRestartClick = {
-                    finishAffinity()
-                    startActivity(Intent(this@CrashActivity, MainActivity::class.java))
-                },
-            )
+            TadamiTheme {
+                val systemUiController = rememberSystemUiController()
+                val statusBarBackgroundColor = MaterialTheme.colorScheme.surface
+                val navbarScrimColor = MaterialTheme.colorScheme.surfaceContainer
+                val isSystemInDarkTheme = isSystemInDarkTheme()
+
+                LaunchedEffect(systemUiController, statusBarBackgroundColor) {
+                    systemUiController.setStatusBarColor(
+                        color = statusBarBackgroundColor,
+                        darkIcons = statusBarBackgroundColor.luminance() > 0.5,
+                        transformColorForLightContent = { Color.Black },
+                    )
+                }
+                LaunchedEffect(systemUiController, isSystemInDarkTheme, navbarScrimColor) {
+                    systemUiController.setNavigationBarColor(
+                        color = navbarScrimColor,
+                        darkIcons = !isSystemInDarkTheme,
+                        navigationBarContrastEnforced = false,
+                        transformColorForLightContent = { Color.Black },
+                    )
+                }
+                CrashScreen(
+                    exception = exception,
+                    onRestartClick = {
+                        finishAffinity()
+                        startActivity(Intent(this@CrashActivity, MainActivity::class.java))
+                    },
+                )
+            }
         }
     }
 }
