b4828b1336e8a24b8554cf0a70371db356248bb5
Sfoucheur <s.foucheur@gmail.com>
2026-07-12 11:28:26 +0200

    Release v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
 
     defaultConfig {
         applicationId = "com.sf.tadami"
-        versionCode = 48
-        versionName = "1.8.4"
+        versionCode = 49
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BUILD_DATE", "\"${getBuildDate()}\"")
